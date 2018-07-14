a14269091cbe49eb1dab820b9355b775572f6b75
pskuza <pskuza@users.noreply.github.com>
2018-07-14 21:21:22 +0200

    test

--- src/providers/cloudflare.php
+++ src/providers/cloudflare.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace pskuza\dyndns\providers;
 
-class cloudflare implements dns
+class cloudflare implements pskuza\dyndns\providers\dns
 {
     public function __construct(\Noodlehaus\Config $config)
     {
--- src/providers/dummy.php
+++ src/providers/dummy.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace pskuza\dyndns\providers;
 
-class dummy implements dns
+class dummy implements pskuza\dyndns\providers\dns
 {
     public function create_record(): bool
     {
